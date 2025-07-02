a6d78a3a2447ce955c2e698d5ee684cba0ae7c9d
DamianToromanovic <d.toromanovic@web.de>
2025-07-02 14:57:20 +0200

    delete job controller

--- routes/company.route.js
+++ routes/company.route.js
@@ -3,6 +3,8 @@ import { isCompany } from "../middleware/role.middleware";
 
 const router = express.Router();
 
-router.get("/", isCompany, addJob);
+router.post("/", isCompany, addJob);
+router.patch("/:id", isCompany, updateJob);
+router.delete("/:id", isCompany, deleteJob);
 
 export default router;
